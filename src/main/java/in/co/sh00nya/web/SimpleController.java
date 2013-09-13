fb2f62add5d54ae21efbf2d313dad7e6fbbe37cf
Sid <siddharth.god@gmail.com>
2013-09-13 12:45:19 +0530

    Text plain post method added

--- src/main/java/in/co/sh00nya/web/SimpleController.java
+++ src/main/java/in/co/sh00nya/web/SimpleController.java
@@ -3,6 +3,7 @@ package in.co.sh00nya.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,6 +14,12 @@ public class SimpleController {
 	public String getDataByIndex(@PathVariable int index, Model model) {
 		model.addAttribute("index", index);
 		return "displaydata";
+	}
+	
+	@RequestMapping(value = "/data/newtext", method = RequestMethod.POST, consumes = "text/plain")
+	public String createNewData(@RequestBody String data, Model model) {
+		model.addAttribute("data", data);
+		return "create_done";
 	}
 
 }
